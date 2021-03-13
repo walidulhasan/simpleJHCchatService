3fc403695b3c96ec17a725f7e6af26caa20c971e
walidulhasn <walidulhasanboniamin@gmail.com>
2021-03-13 11:39:53 +0600

    first commit

---
+++ simpleJHCchatService/app.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    users = [];
+
+app.use('/', express.static(__dirname + '/www'));
+server.listen(8080);
+console.log("Server is running at port 8080");
+
+io.sockets.on('connection', function(socket) {
+    socket.on('login', function(nickname) {
+        if (users.indexOf(nickname) > -1) {
+            socket.emit('nickExisted');
+        } else {
+            socket.nickname = nickname;
+            users.push(nickname);
+            socket.emit('loginSuccess');
+            io.sockets.emit('system', nickname, users.length, 'login');
+        };
+    });
+    socket.on('disconnect', function() {
+        if (socket.nickname != null) {
+            users.splice(users.indexOf(socket.nickname), 1);
+            socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+        }
+    });
+    socket.on('postMsg', function(msg, color) {
+        socket.broadcast.emit('newMsg', socket.nickname, msg, color);
+    });
+    socket.on('img', function(imgData, color) {
+        socket.broadcast.emit('newImg', socket.nickname, imgData, color);
+    });
+});
+
+
